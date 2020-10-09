064639573fe4a32dee90a911f2bb11ac381eec97
Stephen Keenan <stephenkeenan02@gmail.com>
2020-10-09 13:54:16 -0400

    Fourth commit

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
+//Array of questions, choices, and correct answers
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
-      },
-      {
+    },
+    {
         title: "The condition in an if / else statement is enclosed within ___",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
-      },
-      {
+    },
+    {
         title: "Arrays in JavaScript can be used to store ___",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
-      },
-      {
+    },
+    {
         title: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
-      },
-      {
+    },
+    {
         title:
-          "A very useful tool used during development and debugging for printing content to the debugger is:",
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
         answer: "console.log"
-      }
-    ];
+    }
+];
 
+//setting the numerical variables for the functions.. scores and timers.. 
 var score = 0;
 var currentQuestion = -1;
 var timeLeft = 0;
 var timer;
 
+//Starts countdown timer when "Start Quiz" button is clicked
 function start() {
 
     timeLeft = 75;
     document.getElementById("timeLeft").innerHTML = timeLeft;
 
-    time = setInterval(function() {
+    timer = setInterval(function () {
         timeLeft--;
         document.getElementById("timeLeft").innerHTML = timeLeft;
-        if (timeLeft <=0) {
+        //Ends game when time is below 0
+        if (timeLeft <= 0) {
             clearInterval(timer);
             endGame();
         }
@@ -49,12 +53,105 @@ function start() {
     next();
 }
 
+//Stop timer to end game 
 function endGame() {
     clearInterval(timer);
 
     var quizContent = `
-    <h2>All done!</h2>
-    <h3>You got a ` + score +  ` /5</h3>
+    <h2>All Done!</h2>
+    <h3>You got a ` + score + ` /5!</h3>
     <input type="text" id="name" placeholder="Initials"> 
     <button onclick="setScore()">Submit</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//Store score to local storage
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementById('name').value);
+    getScore();
+}
+
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `-</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//Clear score name and value When "Clear Score" is clicked
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName", "");
+
+    resetGame();
+}
+
+//Reset the game 
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        Coding Quiz Challenge
+    </h1>
+    <h3>
+        Try to answer the following code related questions within the time
+        limit. Keep in mind that incorrect answers will penalize your scoreTime
+        by ten seconds. 
+    </h3>
+    <button onclick="start()">Start Quiz</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//Deducts 10 seconds when an incorrect answer is picked
+function incorrect() {
+    timeLeft -= 10;
+    next();
+}
+
+//Increases score by 1 for each correct answer
+function correct() {
+    score += 1;
+    next();
+}
+
+//Loop through questions 
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>";
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+    }
+
+
+    document.getElementById("quizBody").innerHTML = quizContent;
 }

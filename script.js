ab29172445ccc1ef2830f3d28a4cfbac022d3d29
Stephen Keenan <stephenkeenan02@gmail.com>
2020-10-09 19:51:20 -0400

    Tenth commit

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ var timer;
 //Starts countdown timer when "Start Quiz" button is clicked
 function start() {
 
-    timeLeft = 60;
+    timeLeft = 30;
     document.getElementById("timeLeft").innerHTML = timeLeft;
 
     timer = setInterval(function () {
